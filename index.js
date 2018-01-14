1776288e1ca12a59e6713822ff160ed5cc1d0bd6
Pete Gallagher <pete@pjgcreations.co.uk>
2018-01-14 14:23:34 +0000

    Flashing LEDs

--- index.js
+++ index.js
@@ -10,3 +10,20 @@ const wpi = require('wiring-pi');
 // Setup the Wiring Pi System
 wpi.setup('wpi');
 
+// Setup our LED Pin
+wpi.pinMode(0, wpi.OUTPUT);
+wpi.pinMode(16, wpi.OUTPUT);
+
+for (var i = 1; i < 10; i++)
+{
+  wpi.digitalWrite(0, 1);
+  wpi.digitalWrite(16, 1);
+
+  wpi.delay(500);
+
+  wpi.digitalWrite(0, 0);
+  wpi.digitalWrite(16, 0);
+
+  wpi.delay(500);
+
+}

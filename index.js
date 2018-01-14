72ec5cd8eb8e83e0013285027187dd67da2d41ee
Pete Gallagher <pete@pjgcreations.co.uk>
2018-01-14 15:35:37 +0000

    Used Config

--- index.js
+++ index.js
@@ -23,18 +23,18 @@ const wpi = require('wiring-pi');
 wpi.setup('wpi');
 
 // Setup our LED Pin
-wpi.pinMode(0, wpi.OUTPUT);
-wpi.pinMode(16, wpi.OUTPUT);
+wpi.pinMode(config.RedLED, wpi.OUTPUT);
+wpi.pinMode(config.GreenLED, wpi.OUTPUT);
 
 for (var i = 1; i < 10; i++)
 {
-  wpi.digitalWrite(0, 1);
-  wpi.digitalWrite(16, 1);
+  wpi.digitalWrite(config.RedLED, 1);
+  wpi.digitalWrite(config.GreenLED, 1);
 
   wpi.delay(500);
 
-  wpi.digitalWrite(0, 0);
-  wpi.digitalWrite(16, 0);
+  wpi.digitalWrite(config.RedLED, 0);
+  wpi.digitalWrite(config.GreenLED, 0);
 
   wpi.delay(500);
 
